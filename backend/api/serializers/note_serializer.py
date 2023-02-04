8e2f7bacb3983c564ee77a8b0b318767af5cfca8
darkf1 <clemsontigers95@gmail.com>
2023-02-04 17:05:45 -0500

    Remove user references

--- backend/api/serializers/note_serializer.py
+++ backend/api/serializers/note_serializer.py
@@ -6,5 +6,5 @@ from api.models import Note
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        read_only_fields = ["user"]
+        read_only_fields = []
         fields = read_only_fields + ["content", "x_loc", "y_loc"]
--- backend/api/serializers/user_serializer.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from rest_framework import serializers
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        read_only_fields = ["id"]
-        fields = read_only_fields + ["first_name", "last_name", "email"]
