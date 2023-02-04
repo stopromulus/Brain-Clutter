94de9330364b71fb20381a0cc90a681b0a97ff58
Milo Weinberg <iapetus011@gmail.com>
2023-02-04 14:52:26 -0500

    Rename stuff

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,13 +1,10 @@
 <script>
-    import calendarimage from '$lib/images/calendar-image.png';
+    import calenderImagePng from '$lib/images/calendar-image.png';
 </script>
 
 <h1 class="text-center text-2xl">Brain Clutter</h1>
 
-<button>
-    <div class="border-2 500">
-        Calendar 
-        <img src={calendarimage} alt="Calendar Icon" class="w-16">
-    </div>
+<button class="border-2">
+    Calendar
+    <img src={calenderImagePng} alt="Calendar Icon" class="w-16">
 </button>
-
