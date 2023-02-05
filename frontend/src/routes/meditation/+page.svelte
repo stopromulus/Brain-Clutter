aadc089675fa28181946700625a5c310be4d10d8
stopromulus <romestoppiello@gmail.com>
2023-02-05 10:17:14 -0500

    header

--- frontend/src/routes/meditation/+page.svelte
+++ frontend/src/routes/meditation/+page.svelte
@@ -1,2 +1,7 @@
+<script>
+    import Header from "../header.svelte";
+</script>
+
+<Header></Header>
 
 <iframe src="kalm/KalmBG.html" title="meditation" class="w-full h-screen"></iframe>
--- frontend/src/routes/notes/header.svelte
+++
@@ -1,5 +0,0 @@
-<svelte:head>
-    <div class="flex space-x-2 justify-center">
-        <button type="button" class="">Button</button>
-    </div>
-</svelte:head>
