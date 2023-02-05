b1004b67fbc42fb48ede40ff298e4c99c99e2472
stopromulus <romestoppiello@gmail.com>
2023-02-04 20:11:11 -0500

    BUTTTONNNNN

--- frontend/src/routes/iconbutton.svelte
+++ frontend/src/routes/iconbutton.svelte
@@ -2,11 +2,12 @@
     export let name;
     export let src;
     export let alt;
+    export let link="";
 </script>
 
-<button on:click class="center">
-    <div class=" border-solid border-2">
+<a href={link} class="rounded-lg overflow-hidden border-solid border-2 shadow-lg p-2">
+    <div class="center">
         {name} 
         <img src={src} alt={alt} class="w-16">
     </div>
-</button>
+</a>
