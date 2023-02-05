d23a3a7e7c9e3f990cd123c221ab60203c2a1fa7
Milo Weinberg <iapetus011@gmail.com>
2023-02-04 21:02:55 -0500

    Stuff

--- frontend/src/routes/notes/+page.svelte
+++ frontend/src/routes/notes/+page.svelte
@@ -7,23 +7,29 @@
     function toRect({x, y, width, height}) {
         return [x, y, x + width, y + height];
     }
+
+    function addNoteBox(ev) {
+        console.log(ev);
+        let noteBox = {el: null, grab: null, content: '', width: 300, height: 100, x: ev.pageX - 150, y: ev.pageY - 50 };
+
+        const [aX1, aY1, aX2, aY2] = toRect(noteBox);
+
+        if (noteBoxes.filter(nb => {
+            const [bX1, bY1, bX2, bY2] = toRect(nb);
+            return aX2 >= bX1 && aX1 <= bX2 && aY1 <= bY2 && aY2 >= bY1;
+        }).length) return;
+
+        noteBoxes = [...noteBoxes, noteBox];
+        setTimeout(() => {
+            noteBox.el?.focus?.();
+        });
+    }
 </script>
 
-<div class="relative h-screen" on:click|self={(ev) => {
-    let noteBox = {el: null, grab: null, content: '', width: 300, height: 100, x: ev.layerX - 150, y: ev.layerY - 50 };
-
-    const [aX1, aY1, aX2, aY2] = toRect(noteBox);
-
-    if (noteBoxes.filter(nb => {
-        const [bX1, bY1, bX2, bY2] = toRect(nb);
-        return aX2 >= bX1 && aX1 <= bX2 && aY1 <= bY2 && aY2 >= bY1;
-    }).length) return;
-
-    noteBoxes = [...noteBoxes, noteBox];
-    setTimeout(() => {
-        noteBox.el?.focus?.();
-    });
-}}>
+<div class="relative h-screen" on:click|self={addNoteBox}>
+    {#if noteBoxes.length === 0}
+        <p on:click={addNoteBox} class="text-3xl text-gray-400 text-center pt-[40vh] select-none">Click/tap anywhere to make a note!</p>
+    {/if}
     {#each noteBoxes as noteBox, idx}
         <div
             class="border-2 min-h-[2.8rem] border-gray-400 absolute rounded-lg overflow-hidden focus-within:resize focus-within:border-blue-500 p-2 bg-white"
